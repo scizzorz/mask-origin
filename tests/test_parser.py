e4be0ce515a0282b5a10330e9e44405c2a1fe655
John Weachock <jweachock@gmail.com>
2016-07-10 12:42:53 -0400

    Add some parser tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -46,3 +46,25 @@ def test_lt_err1():
 def test_lt_err2():
   ctx = L.context(name_stream(3))
   L.eq(L.number_token).match(ctx)
+
+
+def test_all():
+  ctx = L.context(num_stream(6))
+  assert L.all(L.eq(L.number_token(0))).match(ctx) == [L.number_token(0)]
+  assert L.all(L.eq(L.number_token(1)), L.eq(L.number_token(2))).match(ctx) == [L.number_token(1), L.number_token(2)]
+  assert L.all(L.lt(L.number_token), L.lt(L.number_token), L.lt(L.number_token)).match(ctx) == [L.number_token(3), L.number_token(4), L.number_token(5)]
+
+@xfail(raises=SyntaxError)
+def test_all_err1():
+  ctx = L.context(num_stream(3))
+  L.all(L.lt(L.number_token), L.lt(L.number_token), L.lt(L.name_token)).match(ctx)
+
+def test_any():
+  ctx = L.context(num_stream(3))
+  assert L.any(L.lt(L.number_token), L.lt(L.name_token)).match(ctx) == L.number_token(0)
+  assert L.any(L.eq(L.number_token(1)), L.eq(L.number_token(2))).match(ctx) == L.number_token(1)
+  assert L.any(L.eq(L.number_token(1)), L.eq(L.number_token(2))).match(ctx) == L.number_token(2)
+
+  ctx = L.context(name_stream(2))
+  assert L.any(L.lt(L.number_token), L.lt(L.name_token)).match(ctx) == L.name_token('name_0')
+  assert L.any(L.lt(L.number_token), L.lt(L.name_token)).match(ctx) == L.name_token('name_1')
