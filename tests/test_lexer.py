643bf958a06c8cd9415b0239838f653538a5d6ea
John Weachock <jweachock@gmail.com>
2016-07-11 23:03:40 -0400

    tests: Add token conversion tests

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -1,16 +1,23 @@
 import pymask.lexer as L
 import pytest
 
+def test_conv():
+  assert L.int_token('0').value == 0
+  assert L.float_token('0.5').value == 0.5
+  assert L.bool_token('true').value == True
+  assert L.bool_token('false').value == False
+  assert L.string_token('"false"').value == 'false'
+
 def test_eq():
   assert L.end_token() == L.end_token()
-  assert L.number_token(0) == L.number_token(0)
-  assert L.number_token(0) != L.number_token(1)
-  assert L.number_token(0) != L.name_token('name')
+  assert L.int_token(0) == L.int_token(0)
+  assert L.int_token(0) != L.int_token(1)
+  assert L.int_token(0) != L.name_token('name')
 
 def test_str():
-  assert str(L.number_token) == 'number_token'
-  assert str(L.number_token(0)) == 'number_token(0)'
+  assert str(L.int_token) == 'int_token'
+  assert str(L.int_token(0)) == 'int_token(0)'
 
 def test_repr():
-  assert repr(L.number_token) == '<number_token>'
-  assert repr(L.number_token(0)) == '<number_token(0)>'
+  assert repr(L.int_token) == '<int_token>'
+  assert repr(L.int_token(0)) == '<int_token(0)>'
